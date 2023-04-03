3f8d4b8d064b96786a8633d14dc01079ecb21853
manojkumar021997 <manojkumar020m@gmail.com>
2023-04-03 21:05:16 +0530

    tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { FaAngleDoubleRight } from 'react-icons/fa';
+import './assets/App.css';
+let URL = 'https://course-api.com/react-tabs-project';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    let [company, setcompany ] = useState([]);
+    let [isloding, setloding] = useState(true);
+    let [value, setvalue] = useState(0);
+
+    const fetchcompany = async()=>{
+        const responce = await fetch(URL);
+        const data = await responce.json();
+        console.log(data);
+        setcompany(data);
+        setloding(false);
+    
+    };
+    useEffect(()=>{
+        fetchcompany()
+    },[]);
+    if(isloding){
+        return(<h1>Loding...</h1>)
+        
+    } 
+    const {title, dates, duties} = company[value];
+    // console.log(title);
+    return(
+        <div>
+            <h2>Experience</h2>
+            {company.map((val, index)=>{
+                return <button className="job-btn" key={uuidv4()} onClick={() => {setvalue(index)}}>{val.company}</button>
+            })}
+            <div className="job-date">{dates}</div>
+            <div className="title">{title}</div>
+            {duties.map((description)=>{
+                return(
+                    <>
+                    <FaAngleDoubleRight />
+                    <p key={uuidv4()}>{description}</p>
+                    </>
+                    
+                )
+            })}
+        </div>
+    )
+
+
+    
+};
+
+
 
 export default App;
